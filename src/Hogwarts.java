2ddafcb78dca8f33555bfcc6ea84289f89a20c11
Davinais <david870620@gmail.com>
2016-12-17 03:07:15 +0800

    Set enum Hogwarts

--- src/Hogwarts.java
+++ src/Hogwarts.java
@@ -1,4 +1,23 @@
 public enum Hogwarts
 {
-    
+    GRIFFENDOR(150, 12, 30), HUFFLEPUFF(120, 12, 30), RAVENCLAW(100, 15, 30), SLYTHERIN(100, 10, 40);
+
+    private int maxStamina, intelligence, magicalAffinityPercent;
+    private Hogwarts(int maxStamina, int intelligence, int magicalAffinityPercent) {
+        this.maxStamina = maxStamina;
+        this.intelligence = intelligence;
+        this.magicalAffinityPercent = magicalAffinityPercent;
+    }
+    public int getMaxStamina()
+    {
+        return maxStamina;
+    }
+    public int getInt()
+    {
+        return intelligence;
+    }
+    public int PercentOfMA()
+    {
+        return magicalAffinityPercent;
+    }
 }
