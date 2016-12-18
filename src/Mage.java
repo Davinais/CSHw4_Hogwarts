32e66d075078e8815af1a3188952c335e3f7188f
Davinais <david870620@gmail.com>
2016-12-18 17:58:09 +0800

    Complete the most part of Mage

--- src/Mage.java
+++ src/Mage.java
@@ -1,4 +1,5 @@
 import java.util.HashMap;
+import java.util.Map;
 
 class SpellExp
 {
@@ -18,19 +19,36 @@ class SpellExp
         return exp;
     }
 }
+class StatusTurn
+{
+    private int turn;
+    public StatusTurn(int turn)
+    {
+        this.turn = turn;
+    }
+    public boolean turnDecrement()
+    {
+        return (0 < --turn);
+    }
+}
 public class Mage
 {
-    private HashMap<Spell, SpellExp> atkSpell = new HashMap<>();
-    private HashMap<Spell, SpellExp> defSpell = new HashMap<>();
-    private HashMap<Spell, SpellExp> movSpell = new HashMap<>();
+    private final HashMap<Spell, SpellExp> atkSpells = new HashMap<>();
+    private final HashMap<Spell, SpellExp> defSpells = new HashMap<>();
+    private final HashMap<Spell, SpellExp> movSpells = new HashMap<>();
+    private final HashMap<SpecialEffect, StatusTurn> status = new HashMap<>();
+    private final String NAME;
     private final int STAMINA_MAX, MA_PERCENT;
-    private int intelligence, stamina;
-    public Mage(Hogwarts college)
+    private int intelligence, stamina, potionAmount;
+    private int beDamaged, reduceDamage;
+    public Mage(String name, Hogwarts college)
     {
+        NAME = name;
         STAMINA_MAX = college.getMaxStamina();
         MA_PERCENT = college.percentOfMA();
         intelligence = college.getInt();
         stamina = STAMINA_MAX;
+        potionAmount = 5;
     }
     public boolean learnSpell(Spell spell)
     {
@@ -40,26 +58,26 @@ public class Mage
         {
             case ATTACK:
             {
-                if(atkSpell.containsKey(spell))
-                    atkSpell.get(spell).addExp(spell.getLearnExp());
+                if(atkSpells.containsKey(spell))
+                    atkSpells.get(spell).addExp(spell.getLearnExp());
                 else
-                    atkSpell.put(spell, new SpellExp(spell.getInitialExp()));
+                    atkSpells.put(spell, new SpellExp(spell.getInitialExp()));
                 break;
             }
             case DEFENSE:
             {
-                if(defSpell.containsKey(spell))
-                    defSpell.get(spell).addExp(spell.getLearnExp());
+                if(defSpells.containsKey(spell))
+                    defSpells.get(spell).addExp(spell.getLearnExp());
                 else
-                    defSpell.put(spell, new SpellExp(spell.getInitialExp()));
+                    defSpells.put(spell, new SpellExp(spell.getInitialExp()));
                 break;
             }
             case MOVE:
             {
-                if(movSpell.containsKey(spell))
-                    movSpell.get(spell).addExp(spell.getLearnExp());
+                if(movSpells.containsKey(spell))
+                    movSpells.get(spell).addExp(spell.getLearnExp());
                 else
-                    movSpell.put(spell, new SpellExp(spell.getInitialExp()));
+                    movSpells.put(spell, new SpellExp(spell.getInitialExp()));
                 break;
             }
             default:
@@ -67,5 +85,52 @@ public class Mage
         }
         intelligence += spell.getLearnInt();
         return true;
+    }
+    public boolean usePotion()
+    {
+        if(potionAmount <= 0)
+            return false;
+        potionAmount--;
+        stamina += 50;
+        return true;
+    }
+    public void beCasted(int damage, SpecialEffect effect)
+    {
+        beDamaged = damage;
+        autoCastDefense();
+        stamina -= (beDamaged - reduceDamage);
+        if(!effect.equals(SpecialEffect.NONE) && !status.containsKey(effect))
+            status.put(effect, new StatusTurn(effect.getNeededTurn()));
+    }
+    public void autoCastDefense()
+    {
+        if(status.containsKey(SpecialEffect.DISARMED) || status.containsKey(SpecialEffect.PETRIFIED))
+            return;
+        reduceDamage = 0;
+        for(Map.Entry<Spell, SpellExp> spell : defSpells.entrySet())
+        {
+            int temp = (int)(Math.floor((spell.getKey().getBasicNumber()*0.01)*(1+MA_PERCENT*0.01)*beDamaged));
+            if(temp > reduceDamage)
+                reduceDamage = temp;
+            SpecialEffect spellSE = spell.getKey().getSpecialEffect();
+            if(!(spellSE.equals(SpecialEffect.NONE)) && !(status.containsKey(spellSE)))
+                status.put(spellSE, new StatusTurn(spellSE.getNeededTurn()));
+        }
+    }
+    public boolean isInStatus(SpecialEffect effect)
+    {
+        return status.containsKey(effect);
+    }
+    public String getName()
+    {
+        return NAME;
+    }
+    public int getStamina()
+    {
+        return stamina;
+    }
+    public int getPotionAmount()
+    {
+        return potionAmount;
     }
 }
--- src/SpecialEffect.java
+++ src/SpecialEffect.java
@@ -1,4 +1,14 @@
 public enum SpecialEffect
 {
-    NONE, BLEEDING, PATRONUS, DISARMED ,PETRIFIED;
+    NONE(0), BLEEDING(2), PATRONUS(1), DISARMED(1) ,PETRIFIED(1);
+
+    private int turn;
+    private SpecialEffect(int turn)
+    {
+        this.turn = turn;
+    }
+    public int getNeededTurn()
+    {
+        return turn;
+    }
 }
