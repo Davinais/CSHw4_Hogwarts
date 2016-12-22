4a0f314055c4afa994f2b4fdf3030061a8267244
Davinais <david870620@gmail.com>
2016-12-23 03:07:19 +0800

    Add some messages and commands "pass", "help"

--- src/hw4.java
+++ src/hw4.java
@@ -5,6 +5,29 @@ public class hw4
 {
     private static ArrayList<Mage> players;
     private static ArrayList<Injury> injury;
+
+    public static void printHelp()
+    {
+        System.out.println("可使用指令：");
+        System.out.println("  attack [咒語]：以[咒語]攻擊");
+        System.out.println("  learn [咒語]：學習[咒語]");
+        System.out.println("  recover：使用魔藥回復");
+        System.out.println("  pass：跳過此回合");
+        System.out.println("  game over：結束遊戲");
+        System.out.print("咒語列表：");
+        int lineSpace=0;
+        for(Spell spell : Spell.values())
+        {
+            if(lineSpace == 0)
+            {
+                lineSpace = 3;
+                System.out.print("\n  ");
+            }
+            System.out.print("[" + spell + "]");
+            lineSpace--;
+        }
+        System.out.println();
+    }
     private static boolean takeAction(Mage mage, String[] commands)
     {
         boolean isValid;
@@ -70,6 +93,14 @@ public class hw4
                 else
                     injury.add(Injury.noInjury());
                 break;
+            case "pass":
+                isValid = true;
+                injury.add(Injury.noInjury());
+                break;
+            case "help":
+                printHelp();
+                isValid = false;
+                break;
             default:
                 isValid = false;
                 System.out.println("["+mage.getName()+"]找不到你輸入的指令呢，請確認拼字重新輸入試試看喔");
@@ -84,6 +115,12 @@ public class hw4
         players = new ArrayList<>();
         injury = new ArrayList<>();
         SuddenEventHandler events = new SuddenEventHandler("/text/event.txt");
+        System.out.println("歡迎各位參加巫師鬥法大賽～");
+        System.out.println("本比賽由以下四個可參賽的學院贊助：");
+        System.out.print("  ");
+        for(Hogwarts college : Hogwarts.values())
+            System.out.print("[ " + college + " ] ");
+        System.out.println();
         for(int i=0; i<playerNum; i++)
         {
             boolean error = false;
@@ -113,6 +150,7 @@ public class hw4
         int nowTurn = 0;
         boolean gameEnd = false;
         
+        printHelp();
         Game:
         while(!gameEnd)
         {
@@ -149,6 +187,24 @@ public class hw4
                 gameEnd |= players.get(i).turnEnd(injury.get(injurySource));
             }
         }
+        //檢查勝利者
+        System.out.print("勝利者為：");
+        //先假定勝利者，再來逐一比對
+        Mage winner = players.get(0);
+        //設立一變數表示體力值相等的參賽者，是為了讓以後能方便增加玩家人數才這樣寫的（茶。當此值與玩家人數相等帶所有人平手，除此之外此變數無意義
+        int equalPlayer = 0;
+        for(Mage player : players)
+        {
+            if(player.getStamina() > winner.getStamina())
+                winner = player;
+            else if(player.getStamina() == winner.getStamina())
+                equalPlayer++;
+        }
+        if(equalPlayer == players.size())
+            System.out.println("從缺，兩人平手！");
+        else
+            System.out.println(winner.getName()+"！");
+        System.out.println("感謝各位參加巫師鬥法大賽，期待您們下次的大展身手");
         scanner.close();
     }
 }
