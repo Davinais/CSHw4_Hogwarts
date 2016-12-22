ff73494edb656872ce4c3f9f768084cc16e3d743
Davinais <david870620@gmail.com>
2016-12-23 02:14:07 +0800

    Print event's information

--- src/SuddenEventHandler.java
+++ src/SuddenEventHandler.java
@@ -44,28 +44,38 @@ public class SuddenEventHandler
         switch(events.get(turn))
         {
             case HIPPOGRIF:
+                System.out.println("從鷹馬馬背上摔下來，雙方體力減少15！");
                 for(Mage mage:players)
                 {
                     mage.staminaChange(-15);
                 }
                 break;
             case BERTIEBEANS:
+                System.out.println("吃到鼻涕口味的柏蒂全口味豆，雙方玩家智力減少5！");
                 for(Mage mage:players)
                 {
                     mage.intelligenceChange(-5);
                 }
                 break;
             case FEAST:
+                System.out.println("開學宴會，雙方體力增加30！");
                 for(Mage mage:players)
                 {
                     mage.staminaChange(30);
                 }
                 break;
             case DEMENTOR:
+                System.out.println("催狂魔來襲，需要護法的協助！");
                 for(Mage mage:players)
                 {
+                    mage.patronusDefense();
                     if(!mage.isInStatus(SpecialEffect.PATRONUS))
+                    {
+                        System.out.println("["+mage.getName()+"]"+"沒有護法在旁，直接被吸取靈魂而亡！");
                         mage.instantDeath();
+                    }
+                    else
+                        System.out.println("["+mage.getName()+"]"+"藉助護法，成功抵抗來自催狂魔的攻擊！");
                 }
                 break;
             default:
